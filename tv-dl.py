968e51150817268f7a962992b3a70723b18325f8
kpgx <kpgxyz@gmail.com>
2014-01-13 20:01:11 +0530

    added novamov support

--- tv-dl.py
+++ tv-dl.py
@@ -11,13 +11,19 @@ def find_all(sub, a_str):
         yield start
         start += len(sub)
 
-def get_page(url):	
+def get_page(url):
+	"""
+	get the source page
+	"""	
 	r = scrap.Render(url)  
 	html = r.frame.toHtml()
 	
 	return str(html)	
 	
 def scrape(source):
+	"""
+	scrape the source page and extract file ids for nowvideo hosted videos
+	"""
 	episodes=[]
 	epi_idx=list(find_all('<td class="episode">',source))
 	s_name_s=source.find('<a href="index.html">')+21
@@ -27,7 +33,13 @@ def scrape(source):
 	s_name=source[s_name_s:s_name_e].strip()
 	e_name=source[e_name_s:e_name_e].strip()
 	fpath=[s_name,e_name]
+	c=1
 	for idx in epi_idx:		
+		st=int(idx)
+		if	c<len(epi_idx):
+			end=int(epi_idx[c])
+		else:
+			end=len(source)
 		epi_data={}
 		epi_data['hosts']=[]
 		epi_data['path']=fpath
@@ -38,21 +50,34 @@ def scrape(source):
 		e_name_s=source.find('<b>',int(idx))+3
 		e_name_e=source.find('</b>',int(idx))
 		epi_data['name']=source[e_name_s:e_name_e]
-		e_link_nowvideo_s=source.find('href="http://www.free-tv-video-online.me/player/nowvideo.php?id=',int(idx))+len('href="http://www.free-tv-video-online.me/player/nowvideo.php?id=')	
+		#nowvideo
+		nowvideo='href="http://www.free-tv-video-online.me/player/nowvideo.php?id='
+		e_link_nowvideo_s=source.find(nowvideo,st,end)+len(nowvideo)	
 		e_link_nowvideo_e=e_link_nowvideo_s+13
-		if source.find('href="http://www.free-tv-video-online.me/player/nowvideo.php?id=',int(idx))>0:			
+		if source.find(nowvideo,st,end)>0:			
 			epi_data['hosts'].append('nowvideo')
 			epi_data['link']['nowvideo']=source[e_link_nowvideo_s:e_link_nowvideo_e]
-		#~ e_link_gorilla_s=source.find('http://www.free-tv-video-online.me/player/gorillavid.php?id=',int(idx))+len('http://www.free-tv-video-online.me/player/gorillavid.php?id=')
+		#novamov
+		novamov='href="http://www.free-tv-video-online.me/player/novamov.php?id='
+		e_link_novamov_s=source.find(novamov,st,end)+len(novamov)	
+		e_link_novamov_e=e_link_novamov_s+13
+		if source.find(novamov,st,end)>0:			
+			epi_data['hosts'].append('novamov')
+			epi_data['link']['novamov']=source[e_link_novamov_s:e_link_novamov_e]
+		#uncomment below part when gorilla support is restored
+		#~ e_link_gorilla_s=source.find('http://www.free-tv-video-online.me/player/gorillavid.php?id=',st,end)+len('http://www.free-tv-video-online.me/player/gorillavid.php?id=')
 		#~ e_link_gorilla_e=e_link_gorilla_s+12
-		#~ if source.find('http://www.free-tv-video-online.me/player/gorillavid.php?id=',int(idx))>0:			
+		#~ if source.find('http://www.free-tv-video-online.me/player/gorillavid.php?id=',st,end)>0:			
 			#~ epi_data['hosts'].append('gorilla')
 			#~ epi_data['link']['gorilla']=source[e_link_gorilla_s:e_link_gorilla_e]		
 		episodes.append(epi_data)
-		
+		c+=1
 	return episodes	
 	
 def host_link(episodes):
+	"""
+	create the real links
+	"""
 	for episode in episodes:
 		if len(episode['link'])>0:
 			
@@ -61,6 +86,8 @@ def host_link(episodes):
 					episode['link']['gorilla']='http://gorillavid.in/'+episode['link']['gorilla']
 				if key=='nowvideo':
 					episode['link']['nowvideo']='http://www.nowvideo.ch/video/'+episode['link']['nowvideo']
+				if key=='novamov':
+					episode['link']['novamov']='http://www.novamov.com/video/'+episode['link']['novamov']
 				
 	return episodes
 	
@@ -75,22 +102,36 @@ def read_jason():
 	return json.load(open("data"))
 	
 def d_axel(f_link,f_path):
-	cmd='youtube-dl -g '+f_link+' | xargs axel -a -n 10 -o "'+f_path+'"'	
-	return os.system(cmd)
+	"""
+	download using axel
+	"""
+	cmd='youtube-dl -g '+f_link+' | xargs axel -a -n 10 -o "'+f_path+'"'
+	print cmd
+	return 0
+	#~ return os.system(cmd)
+	
 def d_native(f_link,f_path):
+	"""
+	download using native youtube-dl
+	"""
 	cmd='youtube-dl '+f_link+' -o "'+f_path+'"'
+	#~ print cmd
 	return os.system(cmd)
 	
 
-def download_axel(eps,choices,host):
+def download(eps,choices,host):
+	"""
+	create the command and send it to appropriate downloader
+	"""
 	ep=eps[0]
 	op=os.system('mkdir "'+ep['path'][0]+'" > /dev/null 2> /dev/null')
 	op=os.system('mkdir "'+ep['path'][0]+'/'+ep['path'][1]+'" > /dev/null 2> /dev/null')
 	count =1
 	for ep in eps:
+		hst=host
 		if host not in ep['hosts']:
-			host=ep['hosts'][0]
-		f_link=ep['link'][host]
+			hst=ep['hosts'][0]
+		f_link=ep['link'][hst]
 		f_path=ep['path'][0]+'/'+ep['path'][1]+'/'+ep['name']		
 		if choices is not None and len(choices)>0:             
 			if ep['id'][1:] in choices:
@@ -100,8 +141,7 @@ def download_axel(eps,choices,host):
 				
 		else:
 			print('Downloading file (%d/%d)...  '%(count,len(eps)))				
-			rv=d_axel(f_link,f_path)
-			#~ rv=d_native(f_link,f_path)
+			rv=d_axel(f_link,f_path)			
 		if rv!=0:
 			return 1		
 		count +=1
@@ -117,7 +157,9 @@ def main(**kwargs):
 		print("web page load failed or no downloadable links in web page!")
 		return 1        
 	host_data=host_link(rough_data)
-	download_axel(host_data,kwargs['e'],host)
+	#~ for i in host_data:
+		#~ print i
+	download(host_data,kwargs['e'],host)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TV series downloader.currently supports only nowvideo host only')
